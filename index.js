d5506cce9b692f8b7ae925e8de91e0888169d160
Rob Whiting <rob.t.whiting@gmail.com>
2018-05-19 21:00:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function yourAnimal() {
 function add2(n) {
   return n + two
 
-  // Feel free to move things around!
   const two = 2
 }
+
 
 var funkyFunction = function() {
   return function() {
