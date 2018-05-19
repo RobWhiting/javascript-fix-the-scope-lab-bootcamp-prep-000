bf6df91d389f81d30ef3d27abaa6b8e2b9de0733
Rob Whiting <rob.t.whiting@gmail.com>
2018-05-19 21:30:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ var funkyFunction = function() {
   }
 }
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
+
 var theFunk = funkyFunction()
 
 console.log(funkyFunction()())
+
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// NOTE: you only need to modify the code below this line.
